619cc2c9b43a215fe7369c4d3ec5782c0546cee0
DevAlfonsoCIO <87680723+marpez4@users.noreply.github.com>
2023-02-10 15:06:51 -0600

    Update servicios.modelo.php
    Orden desc en mostrar registros

--- modelos/servicios.modelo.php
+++ modelos/servicios.modelo.php
@@ -77,7 +77,7 @@ class ModeloServicios
 			return $stmt->fetch();
 		} else {
 
-			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla ORDER BY id DESC");
 
 			$stmt->execute();
 
